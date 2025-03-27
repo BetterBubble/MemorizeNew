1f1115f8cb0d0a568e156f754400b8ca809673a9
BetterBubble <sasha_shulga0507@icloud.com>
2025-03-27 21:51:09 +0300

    Начало создания архитектуры MVVM

---
+++ MemorizeNew/MemorizeNew/EmojiMemoryGameNew.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGameNew.swift
+//  MemorizeNew
+//
+//  Created by Александр Шульга on 27.03.2025.
+//
+
+import Foundation
---
+++ MemorizeNew/MemorizeNew/MemorizeGameNew.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGameNew.swift
+//  MemorizeNew
+//
+//  Created by Александр Шульга on 27.03.2025.
+//
+
+import Foundation
