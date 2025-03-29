f6aaa37931d86d49383a673d36a8ab7dc9510be3
BetterBubble <sasha_shulga0507@icloud.com>
2025-03-29 10:44:20 +0300

    Добавлен адаптивный вид карточек, размер карточек подстраивается под их колличестов

--- MemorizeNew/MemorizeNew/EmojiMemoryGameViewNew.swift
+++ MemorizeNew/MemorizeNew/EmojiMemoryGameViewNew.swift
@@ -15,10 +15,8 @@ struct EmojiMemoryGameViewNew: View {
     
     var body: some View {
         VStack {
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            cards
+                .animation(.default, value: viewModel.cards)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -27,17 +25,44 @@ struct EmojiMemoryGameViewNew: View {
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRatio: 2/3
+            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
             }
         }
         .foregroundColor(.orange)
+    }
+    
+    func gridItemWidthThatFits (
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
 
@@ -63,8 +88,8 @@ struct CardView: View {
             
             base.fill()
                 .opacity(card.isFaceUp ? 0 : 1)
-            
         }
+        .opacity(card.isFaceUp || !card.isMatchedl ? 1 : 0)
     }
 }
 
